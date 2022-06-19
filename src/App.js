aa2bcabb6736fd6776b85798621d94416e4409d7
kamal <Adelabuadelek@github.com>
2022-06-19 17:32:06 -0300

    basic react template set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Let's build the MERN TINDER clone</h1>
+      {/* Header */}
+      {/* TindeCards */}
+      {/* SwipButtons */}
     </div>
   );
 }
