b997db3c9e1befa4f08697a6ddc0821e39cef49a
kamal <Adelabuadelek@github.com>
2022-06-19 22:06:12 -0300

    footer buttons added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from './Header';
 import TinderCards from './TinderCards';
+import SwipeButtons from './SwipeButtons'
 
 function App() {
   return (
     <div className="app">
       <Header />
       <TinderCards />
-      {/* SwipButtons */}
+      <SwipeButtons />
     </div>
   );
 }
